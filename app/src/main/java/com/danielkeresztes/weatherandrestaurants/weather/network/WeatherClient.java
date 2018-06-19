b00cdb17fd53b87cd7d6c67a285dda0949c70b4a
Daniel <daniel.keresztes@supercharge.io>
2018-06-19 23:21:53 +0200

    Add okhttp logging

--- app/src/main/java/com/danielkeresztes/weatherandrestaurants/weather/network/WeatherClient.java
+++ app/src/main/java/com/danielkeresztes/weatherandrestaurants/weather/network/WeatherClient.java
@@ -1,6 +1,8 @@
 package com.danielkeresztes.weatherandrestaurants.weather.network;
 
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,9 +19,14 @@ public class WeatherClient {
 
     public static WeatherApi getWeatherApi(){
         if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
             retrofit = new Retrofit
                     .Builder()
                     .baseUrl(API_URL)
+                    .client(client)
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
